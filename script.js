289ffa912397f5cc96cdd3f4d9117076adab57aa
kumilachew-g <kumilachew1212@gmail.com>
2022-07-19 23:47:14 +0300

    fix click menu issues

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
+const menu = document.querySelector('#union-menu');
+const close = document.querySelector('#nav-button-close');
+const navs = document.querySelector('#nav-menu');
+const links = document.querySelectorAll('#nav-menu li');
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
+close.addEventListener('click', () => {
+  navs.classList.toggle('hidden');
 });
 
-document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  navMenu.classList.remove('active');
+menu.addEventListener('click', () => {
+  navs.classList.toggle('hidden');
+});
+
+links.forEach((ele) => ele.addEventListener('click', () => {
+  navs.classList.toggle('hidden');
 }));
+
+const bio = document.querySelector('#demo');
+const about = document.querySelector('#work-section');
+const activeMenuNav = document.querySelectorAll('#desk-list > li > a');
+
+document.addEventListener('scroll', () => {
+  if (window.pageYOffset < bio.offsetHeight + bio.offsetTop) {
+    activeMenuNav.forEach((e) => e.classList.remove('active'));
+    activeMenuNav[0].classList.add('active');
+  } else if (window.pageYOffset < about.offsetHeight + about.offsetTop - 200) {
+    activeMenuNav.forEach((e) => e.classList.remove('active'));
+    activeMenuNav[1].classList.add('active');
+  } else {
+    activeMenuNav.forEach((e) => e.classList.remove('active'));
+    activeMenuNav[2].classList.add('active');
+  }
+});
